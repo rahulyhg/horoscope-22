17b693b8af0eb9f050631e5e06d1cdf15aa64be2
sonicpro <sonic@ukrpost.net>
2015-02-23 14:31:12 +0200

    "form1" div had been changed to form. The action attribute had been added.

--- java script/all java scripts.js
+++ java script/all java scripts.js
@@ -1,16 +1,7 @@
 ﻿var nameZod='null';
 
-function submitButtonClick () {
-    if (isnumber (frames[0].window.document.getElementById("day").value)) {
-        alert("Day: " + frames[0].window.document.getElementById("day").value);
-    }
-    else {
-        alert("Please enter a valid day number.");
-    }
-}
-
 window.onload = function () {
-    frames[0].window.document.getElementById("submitButton1").addEventListener('click', submitButtonClick, false);
+    frames[0].window.document.getElementById("submitButton1").addEventListener('click', analize, false);
 };
 
 function isnumber(str)
@@ -37,30 +28,30 @@ function isnumber(str)
   function analize()
   {
     var b=false;
-    var form = frames[0].window.document.forms[0];
-	var id=form.getElementById("month").selectedIndex;
+    var form = frames[0].window.document.forms["form1"];
+	var id=form["month"].selectedIndex;
 
 	if(nameZod=='null')
 	  alert("Выберите тип гороскопа!");
 	else
 	{
-    if(((nameZod=='Neast')&&(form.getElementById("year").value==""))||((nameZod=='nzodiak')&&(form.getElementById("day").value==""))||((nameZod=='Nnumeric')&&((form.getElementById("day").value=="")||(form.getElementById("year").value==""))))
+    if(((nameZod=='Neast')&&(form["year"].value==""))||((nameZod=='nzodiak')&&(form["day"].value==""))||((nameZod=='Nnumeric')&&((form["day"].value=="")||(form["year"].value==""))))
         alert("Заполните необходимые поля для данного типа гороскопа!");
            else
            {
-           if((!isnumber(form.getElementById("day").value))||(!isnumber(form.getElementById("year").value)))
+           if((!isnumber(form["day"].value))||(!isnumber(form["year"].value)))
                   alert("Данные вводятся цифрами!");
            else
            {
-            if((form.getElementById("day").value<1)||(form.getElementById("day").value>31))
+            if((form["day"].value<1)||(form["day"].value>31))
                alert("Такого дня не существует!");
             else
             {
-               if((form.getElementById("day").value==31)&&((id==3)||(id==5)||(id==8)||(id==10)))
+               if((form["day"].value==31)&&((id==3)||(id==5)||(id==8)||(id==10)))
                    alert("В этом месяце 30 дней!");
                else
                {
-                if((form.getElementById("day").value>=29)&&(id==1)&&(!FindVisokosnYear(form.getElementById("year").value)))
+                if((form["day"].value>=29)&&(id==1)&&(!FindVisokosnYear(form["year"].value)))
                     alert("В этом месяце 28 дней!");
                 else
                   b=true;
@@ -70,7 +61,7 @@ function isnumber(str)
         }
     }
     return b;
-}		
+}
 
 
 function findYear(yearZod, inputYear)
@@ -104,53 +95,53 @@ function findVisokosnYear(inputYear)
 
 function eastYear()
 {
-	if(findYear("1992", form.getElementById("year").value)==true)
+	if(findYear("1992", form["year"].value)==true)
 
 
         top.frames[1].location='East/Monkey.html';
        else
          {
-	   if(findYear("1993", form.getElementById("year").value)==true)
+	   if(findYear("1993", form["year"].value)==true)
 	    top.frames[1].location='East/Cock.html';
 	    else
             {
-	      if(findYear("1994", form.getElementById("year").value)==true)
+	      if(findYear("1994", form["year"].value)==true)
 	       top.frames[1].location='East/Dog.html';
 	       else
                {
-	         if(findYear("1995", form.getElementById("year").value)==true)
+	         if(findYear("1995", form["year"].value)==true)
 	          top.frames[1].location='East/Pig.html';
 	          else
                   {
-	            if(findYear("1996", form.getElementById("year").value)==true)
+	            if(findYear("1996", form["year"].value)==true)
 	             top.frames[1].location='East/Mouse.html';
 	             else
                      {
-	              if(findYear("1997", form.getElementById("year").value)==true)
+	              if(findYear("1997", form["year"].value)==true)
 	                top.frames[1].location='East/Bull.html';
 	                else
                         {
-	                 if(findYear("1998", form.getElementById("year").value)==true)
+	                 if(findYear("1998", form["year"].value)==true)
 	                   top.frames[1].location='East/Tiger.html';
 	                  else
                           {
-	                    if(findYear("1999", form.getElementById("year").value)==true)
+	                    if(findYear("1999", form["year"].value)==true)
 	                      top.frames[1].location='East/Rabbit.html';
 	                     else
-                             { 
-	                       if(findYear("2000", form.getElementById("year").value)==true)
+                             {
+	                       if(findYear("2000", form["year"].value)==true)
 	                         top.frames[1].location='East/Dragon.html';
 	                       else
                                {
-	                          if(findYear("2001", form.getElementById("year").value)==true)
+	                          if(findYear("2001", form["year"].value)==true)
 	                            top.frames[1].location='East/Snake.html';
 	                          else
                                   {
-	                             if(findYear("2002", form.getElementById("year").value)==true)
+	                             if(findYear("2002", form["year"].value)==true)
 	                               top.frames[1].location='East/Horse.html';
 	                            else
                                     {
-	                                if(findYear("2003", form.getElementById("year").value)==true)
+	                                if(findYear("2003", form["year"].value)==true)
 	                                  top.frames[1].location='East/Goat.html';
                                     }
                                          }
